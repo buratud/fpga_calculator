9a2f3e03c81fa8fea09fdcd7b9e24996c5ac1cbc
buratud <62024314+buratud@users.noreply.github.com>
2023-10-04 05:19:16 +0000

    Change sensitivity test to clock

--- SelectNum.vhd
+++ SelectNum.vhd
@@ -5,6 +5,7 @@ ENTITY SelectNum IS
         N : INTEGER := 5
     );
     PORT (
+        clk : IN STD_LOGIC;
         state : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
         a_i, b_i : IN STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
         a_o, b_o : OUT STD_LOGIC_VECTOR(N - 1 DOWNTO 0)
@@ -14,7 +15,7 @@ END ENTITY;
 
 ARCHITECTURE flow OF SelectNum IS
 BEGIN
-    PROCESS (state)
+    PROCESS (clk)
     BEGIN
         IF (state = "00") THEN
             a_o <= a_i;
