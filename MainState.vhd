2605d4795a96c0cac8511da20baf327498383fe0
reconnecting <tapanawat2004@gmail.com>
2023-10-09 11:23:01 +0700

    Change main state to be trigger by unpush

--- MainState.vhd
+++ MainState.vhd
@@ -10,20 +10,32 @@ END ENTITY;
 ARCHITECTURE behaivioral OF MainState IS
 	TYPE state_type IS (S_N, S_O, S_F);
 	SIGNAL state : state_type := S_N;
+	SIGNAL last : STD_LOGIC := i;
+	SIGNAL lastRst : STD_LOGIC := rst;
 BEGIN
-	PROCESS (rst, i)
+	PROCESS (clk)
 	BEGIN
-		IF rst = '1' THEN
-			state <= S_N;
-		ELSIF rising_edge(i) THEN
-			CASE state IS
-				WHEN S_N =>
-					state <= S_O;
-				WHEN S_O =>
-					state <= S_F;
-				WHEN S_F =>
-					state <= S_F;
-			END CASE;
+		IF rising_edge(clk) THEN
+			IF rst = '0' AND lastRst = '1' THEN
+				state <= S_N;
+			ELSE
+				CASE state IS
+					WHEN S_N =>
+						IF i = '0' AND last = '1' THEN
+							state <= S_O;
+						END IF;
+					WHEN S_O =>
+						IF i = '0' AND last = '1' THEN
+							state <= S_F;
+						END IF;
+					WHEN S_F =>
+						IF i = '0' AND last = '1' THEN
+							state <= S_F;
+						END IF;
+				END CASE;
+			END IF;
+			lastRst <= rst;
+			last <= i;
 		END IF;
 	END PROCESS;
 	WITH state SELECT
