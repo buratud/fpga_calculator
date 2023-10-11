69de235c7629426dadea7b95f770446453ce3907
Buratud Thumboon <62024314+buratud@users.noreply.github.com>
2023-10-11 13:11:14 +0700

    Rollback main state

--- MainState.vhd
+++ MainState.vhd
@@ -8,36 +8,38 @@ ENTITY MainState IS
 END ENTITY;
 
 ARCHITECTURE behaivioral OF MainState IS
-	TYPE state_type IS (SN, SO, SF);
-	SIGNAL state : state_type := SN;
+	TYPE state_type IS (S_N, S_O, S_F);
+	SIGNAL state : state_type := S_N;
 	SIGNAL last : STD_LOGIC := i;
+	SIGNAL lastRst : STD_LOGIC := rst;
 BEGIN
-	PROCESS (clk, rst)
+	PROCESS (clk)
 	BEGIN
-		IF rst = '1' THEN
-			state <= SN;
-		ELSIF rising_edge(clk) THEN
-			CASE state IS
-				WHEN SN =>
-					IF i = '0' AND last = '1' THEN
-						state <= SO;
-					ELSE
-						o <= "00";
-					END IF;
-				WHEN SO =>
-					IF i = '0' AND last = '1' THEN
-						state <= SF;
-					ELSE
-						o <= "01";
-					END IF;
-				WHEN SF =>
-					IF i = '0' AND last = '1' THEN
-						state <= SF;
-					ELSE
-						o <= "10";
-					END IF;
-			END CASE;
+		IF rising_edge(clk) THEN
+			IF rst = '0' AND lastRst = '1' THEN
+				state <= S_N;
+			ELSE
+				CASE state IS
+					WHEN S_N =>
+						IF i = '0' AND last = '1' THEN
+							state <= S_O;
+						END IF;
+					WHEN S_O =>
+						IF i = '0' AND last = '1' THEN
+							state <= S_F;
+						END IF;
+					WHEN S_F =>
+						IF i = '0' AND last = '1' THEN
+							state <= S_F;
+						END IF;
+				END CASE;
+			END IF;
+			lastRst <= rst;
 			last <= i;
 		END IF;
 	END PROCESS;
+	WITH state SELECT
+		o <= "00" WHEN S_N,
+		"01" WHEN S_O,
+		"10" WHEN OTHERS;
 END ARCHITECTURE;
